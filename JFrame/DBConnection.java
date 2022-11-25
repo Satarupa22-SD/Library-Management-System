6d768c3acdcf1e15e246909ab29bbe0531798d2b
Satarupa Deb <84276219+Satarupa22-SD@users.noreply.github.com>
2022-11-25 19:15:44 +0530

    Add files via upload

---
+++ JFrame/DBConnection.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package JFrame;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+/**
+ *
+ * @author dell
+ */
+public class DBConnection {
+    
+    static Connection con = null;
+    
+    public static Connection getConnection(){
+        
+         try{
+            Class.forName("com.mysql.jdbc.Driver");
+
+            con =  DriverManager.getConnection("jdbc:mysql://localhost:3306/library_ms","root","");
+            
+            
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+         
+         return con;
+    }
+}
